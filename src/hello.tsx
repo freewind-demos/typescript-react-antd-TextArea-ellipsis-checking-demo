fd336ddaba65be511c5afc85b52790bb6d43486e
Freewind <nowindlee@gmail.com>
2023-11-03 22:03:36 +0800

    typescript-react-antd-textarea-demo

--- src/hello.tsx
+++ src/hello.tsx
@@ -1,12 +1,7 @@
 import React from 'react'
-import {Alert, message} from 'antd';
+import {Input} from 'antd';
 
 export default function Hello() {
-    function showMessage(text: string) {
-        message.destroy();
-        // FIXME we have to put 'message.info' in a 'setTimeout', otherwise it doesn't show on the first click
-        setTimeout(() => message.info(text), 0)
-    }
 
-    return <button onClick={() => showMessage('Hello')}>Show message</button>
+    return <Input.TextArea defaultValue={'sdf'}/>
 };
